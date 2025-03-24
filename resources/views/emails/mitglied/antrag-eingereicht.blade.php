c5fb35806bbe68b9c1c9ed61d97281b68238fb3b
McNamara <holgerehrmann@gmail.com>
2025-03-24 18:31:11 +0100

    Aktualisiere E-Mail-Benachrichtigung für Mitgliedsanträge: Präzisiere den Prüfungsprozess und verbessere die Formulierung.

--- resources/views/emails/mitglied/antrag-eingereicht.blade.php
+++ resources/views/emails/mitglied/antrag-eingereicht.blade.php
@@ -1,15 +1,15 @@
 <x-mail::message>
-    # Wundan zum Gruße, lieber {{ $user->vorname }}!
+# Wundan zum Gruße, {{ $user->vorname }}!
 
-    Dein Mitgliedschaftsantrag wird nun geprüft. Dies kann bis zu drei Tage dauern. Wir informieren dich, sobald dein Antrag bearbeitet wurde und senden dir dann auch direkt die Zahlungsdaten für den Mitgliedsbeitrag zu.
+Dein Mitgliedschaftsantrag wird nun geprüft. Dies geschieht manuell. Lass uns etwas Zeit um deinen Antrag zu prüfen. Wir informieren dich, sobald dein Antrag bearbeitet wurde und senden dir dann auch direkt die Zahlungsdaten für den Mitgliedsbeitrag zu.
     
-    Bis es so weit ist bestätige schon mal deinen Account, indem du auf den folgenden Button klickst.
+Bis es so weit ist bestätige schon mal deinen Account, indem du auf den folgenden Button klickst.
 
-    <x-mail::button :url="$verificationUrl" color="success">Mailadresse bestätigen</x-mail::button>
+<x-mail::button :url="$verificationUrl" color="success">Mailadresse bestätigen</x-mail::button>
 
-    Tuma sa feesa,
+Tuma sa feesa,
 
-    Der Vorstand des OMXFC
+Der Vorstand des OMXFC
 
-    Tanja, Arndt und Markus
+Tanja, Arndt und Markus
 </x-mail::message>
