b38693e2007461faefec4ef876e8f9dc54c400c7
McNamara <holgerehrmann@gmail.com>
2025-04-02 13:46:41 +0200

    Aktualisiere MeetingController: Entferne feste Zoom-Links und verwende Umgebungsvariablen für die Links

--- app/Http/Controllers/MeetingController.php
+++ app/Http/Controllers/MeetingController.php
@@ -15,32 +15,28 @@ class MeetingController extends Controller
                 'day' => 'third monday',
                 'time_from' => '20:00',
                 'time_to' => '20:30',
-                'link' => 'https://fh-potsdam.zoom-x.de/j/67239495040?pwd=E4HHW3KUsEYeaR2mH8iQ28mTOlndYj.1',
             ],
             [
                 'name' => 'AG Fanhörbücher',
                 'day' => 'second wednesday',
                 'time_from' => '19:00',
                 'time_to' => '19:30',
-                'link' => 'https://fh-potsdam.zoom-x.de/j/66231037369?pwd=87ZTXwcH0bDGfXL03RPxxnQ5zSdFDr.1',
             ],
             [
                 'name' => 'AG MAPDRAX',
                 'day' => 'first wednesday',
                 'time_from' => '20:30',
                 'time_to' => '21:00',
-                'link' => 'https://fh-potsdam.zoom-x.de/j/61368594487?pwd=9cv8mhDMrHYMGI6t4bdCH3Mbgzo6mQ.1',
             ],
             [
                 'name' => 'MADDRAX-Stammtisch',
                 'day' => 'see_note',
                 'time_from' => '20:00',
                 'time_to' => null,
-                'link' => 'https://fh-potsdam.zoom-x.de/j/68623833818?pwd=0s93Fwg5o3i936FZbrnqPhE8bkLjuh.1',
             ],
         ];
 
-        // Optional: Nächstes Datum für reguläre Wochentags-Meetings berechnen
+        // Nächstes Datum für reguläre Wochentags-Meetings berechnen
         foreach ($meetings as &$meeting) {
             if ($meeting['day'] !== 'see_note') {
                 $meeting['next'] = Carbon::parse("{$meeting['day']} of this month")->isFuture()
@@ -63,10 +59,10 @@ class MeetingController extends Controller
 
     // Mapping von IDs zu echten Zoom-URLs (auf dem Server!)
     $links = [
-        'maddraxikon' => 'https://fh-potsdam.zoom-x.de/j/67239495040?pwd=E4HHW3KUsEYeaR2mH8iQ28mTOlndYj.1',
-        'fanhoerbuch' => 'https://fh-potsdam.zoom-x.de/j/66231037369?pwd=87ZTXwcH0bDGfXL03RPxxnQ5zSdFDr.1',
-        'mapdrax' => 'https://fh-potsdam.zoom-x.de/j/61368594487?pwd=9cv8mhDMrHYMGI6t4bdCH3Mbgzo6mQ.1',
-        'stammtisch' => 'https://fh-potsdam.zoom-x.de/j/68623833818?pwd=0s93Fwg5o3i936FZbrnqPhE8bkLjuh.1',
+        'maddraxikon' => env('ZOOM_LINK_MADDRAXIKON'),
+        'fanhoerbuch' => env('ZOOM_LINK_HOERBUECHER'),
+        'mapdrax' => env('ZOOM_LINK_MAPDRAX'),
+        'stammtisch' => env('ZOOM_LINK_STAMMTISCH'),
     ];
 
     if (!array_key_exists($meeting, $links)) {
