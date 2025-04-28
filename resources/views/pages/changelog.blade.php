b7ac983740012796b23d03bf835b20c2c209ea7a
McNamara <holgerehrmann@gmail.com>
2025-04-28 16:08:06 +0200

    Aktualisiere die Version im Changelog auf 0.9.0

--- resources/views/pages/changelog.blade.php
+++ resources/views/pages/changelog.blade.php
@@ -4,7 +4,7 @@
         <p class="mb-6">Auf dieser Seite werden sämtliche Änderungen an der Vereinswebseite des Offiziellen Maddrax
             Fanclub e. V. dokumentiert.</p>
         <section class="mb-6">
-            <h2 class="text-xl font-semibold mb-2">Version 0.8.0 (27.04.2025)</h2>
+            <h2 class="text-xl font-semibold mb-2">Version 0.9.0 (xx.05.2025)</h2>
             <h3 class="text-l font-semibold mb-2">Features:</h3>
             <ul class="list-disc ml-6 mt-2">
                 <li>???</li>
