3fb4a2490661c49e842606412beda5b067f9a793
McNamara <holgerehrmann@gmail.com>
2025-05-24 13:09:31 +0200

    Aktualisiere das UpdateProfileInformationForm, um die Benutzerprofilinformationen präziser zu laden und die Löschfunktion für Profilfotos zu integrieren.

--- app/Livewire/Profile/UpdateProfileInformationForm.php
+++ app/Livewire/Profile/UpdateProfileInformationForm.php
@@ -10,14 +10,47 @@ class UpdateProfileInformationForm extends Component
 {
     use WithFileUploads;
 
+    /**
+     * The component's state.
+     *
+     * @var array
+     */
     public $state = [];
+
+    /**
+     * The new avatar for the user.
+     *
+     * @var mixed
+     */
     public $photo;
 
+    /**
+     * Prepare the component.
+     *
+     * @return void
+     */
     public function mount()
     {
-        $this->state = auth()->user()->withoutRelations()->toArray();
+        $this->state = auth()->user()->only([
+            'vorname',
+            'nachname',
+            'email',
+            'strasse',
+            'hausnummer',
+            'plz',
+            'stadt',
+            'land',
+            'telefon',
+            'mitgliedsbeitrag'
+        ]);
     }
 
+    /**
+     * Update the user's profile information.
+     *
+     * @param  \Laravel\Fortify\Contracts\UpdatesUserProfileInformation  $updater
+     * @return void
+     */
     public function updateProfileInformation(UpdatesUserProfileInformation $updater)
     {
         $this->resetErrorBag();
@@ -25,8 +58,8 @@ class UpdateProfileInformationForm extends Component
         $updater->update(
             auth()->user(),
             $this->photo
-                ? array_merge($this->state, ['photo' => $this->photo])
-                : $this->state
+            ? array_merge($this->state, ['photo' => $this->photo])
+            : $this->state
         );
 
         if (isset($this->photo)) {
@@ -37,6 +70,11 @@ class UpdateProfileInformationForm extends Component
         $this->dispatch('refresh-navigation-menu');
     }
 
+    /**
+     * Delete user's profile photo.
+     *
+     * @return void
+     */
     public function deleteProfilePhoto()
     {
         auth()->user()->deleteProfilePhoto();
@@ -44,6 +82,21 @@ class UpdateProfileInformationForm extends Component
         $this->dispatch('refresh-navigation-menu');
     }
 
+    /**
+     * Get the current user of the application.
+     *
+     * @return mixed
+     */
+    public function getUserProperty()
+    {
+        return auth()->user();
+    }
+
+    /**
+     * Render the component.
+     *
+     * @return \Illuminate\View\View
+     */
     public function render()
     {
         return view('profile.update-profile-information-form');
