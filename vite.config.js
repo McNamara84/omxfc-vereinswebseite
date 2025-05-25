02642e3a9a0fe9f208b3e2c0e5e6f40b7057889e
McNamara <holgerehrmann@gmail.com>
2025-05-25 04:30:24 +0200

    Füge maddraxiversum.js zur Eingabe in der Vite-Konfiguration hinzu

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/maddraxiversum.js',],
             refresh: true,
             // Explizit den public-Pfad setzen
             publicDirectory: 'public',
