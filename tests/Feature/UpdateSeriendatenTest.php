565f8541a49ab6028bc200515aa6d521ad961ac7
McNamara <holgerehrmann@gmail.com>
2025-07-21 04:54:39 +0200

    Add tests for updating user Seriendaten
    Introduces feature tests for the UpdateSeriendatenForm Livewire component, covering retrieval, updating, and validation of user Seriendaten fields.

---
+++ tests/Feature/UpdateSeriendatenTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use App\Models\User;
+use Livewire\Livewire;
+use App\Livewire\Profile\UpdateSeriendatenForm;
+
+class UpdateSeriendatenTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_current_seriendaten_are_available(): void
+    {
+        $this->actingAs($user = User::factory()->create([
+            'einstiegsroman' => '1 - Roman',
+            'lesestand' => '2 - Roman',
+            'lieblingsroman' => '3 - Roman',
+            'lieblingsfigur' => 'Figur',
+            'lieblingsmutation' => 'Mutation',
+            'lieblingsschauplatz' => 'Ort',
+            'lieblingsautor' => 'Autor',
+            'lieblingszyklus' => 'Zyklus',
+        ]));
+
+        $component = Livewire::test(UpdateSeriendatenForm::class);
+
+        $this->assertSame('1 - Roman', $component->state['einstiegsroman']);
+        $this->assertSame('2 - Roman', $component->state['lesestand']);
+        $this->assertSame('3 - Roman', $component->state['lieblingsroman']);
+        $this->assertSame('Figur', $component->state['lieblingsfigur']);
+        $this->assertSame('Mutation', $component->state['lieblingsmutation']);
+        $this->assertSame('Ort', $component->state['lieblingsschauplatz']);
+        $this->assertSame('Autor', $component->state['lieblingsautor']);
+        $this->assertSame('Zyklus', $component->state['lieblingszyklus']);
+    }
+
+    public function test_seriendaten_can_be_updated(): void
+    {
+        $this->actingAs($user = User::factory()->create());
+
+        Livewire::test(UpdateSeriendatenForm::class)
+            ->set('state', [
+                'einstiegsroman' => 'A',
+                'lesestand' => 'B',
+                'lieblingsroman' => 'C',
+                'lieblingsfigur' => 'D',
+                'lieblingsmutation' => 'E',
+                'lieblingsschauplatz' => 'F',
+                'lieblingsautor' => 'G',
+                'lieblingszyklus' => 'H',
+            ])
+            ->call('updateSeriendaten');
+
+        $user->refresh();
+
+        $this->assertSame('A', $user->einstiegsroman);
+        $this->assertSame('B', $user->lesestand);
+        $this->assertSame('C', $user->lieblingsroman);
+        $this->assertSame('D', $user->lieblingsfigur);
+        $this->assertSame('E', $user->lieblingsmutation);
+        $this->assertSame('F', $user->lieblingsschauplatz);
+        $this->assertSame('G', $user->lieblingsautor);
+        $this->assertSame('H', $user->lieblingszyklus);
+    }
+
+    public function test_validation_fails_for_too_long_values(): void
+    {
+        $this->actingAs($user = User::factory()->create());
+
+        $long = str_repeat('x', 256);
+
+        Livewire::test(UpdateSeriendatenForm::class)
+            ->set('state', ['einstiegsroman' => $long])
+            ->call('updateSeriendaten')
+            ->assertHasErrors(['einstiegsroman']);
+
+        $this->assertNull($user->fresh()->einstiegsroman);
+    }
+}
