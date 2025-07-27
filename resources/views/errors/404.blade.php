dc3f33678e966fe2a07e6af9f52a3f4f0d4f324c
McNamara <holgerehrmann@gmail.com>
2025-07-27 03:20:02 +0200

    Add custom 404 error image to error page
    Introduced a new 404.png image and updated the 404 error view to display it, enhancing the user experience for not found pages.

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,6 +1,7 @@
 <x-app-layout>
     <div class="container mx-auto py-12 text-center">
         <h1 class="text-5xl font-bold text-[#8B0116] dark:text-[#ff4b63] mb-6">404</h1>
+        <img src="{{ asset('images/errors/404.png') }}" alt="Seite verschollen" class="mx-auto w-64 h-auto mb-6">
         <p class="text-xl text-gray-700 dark:text-gray-300 mb-8">
             {{ __('Wir haben Archivar Zuul losgeschickt um diese Seite zu finden, aber er wurde nicht mal in seiner BagBox fündig. Bitte prüfe die eingegebene URL auf Fehlertaratzen!') }}
         </p>
