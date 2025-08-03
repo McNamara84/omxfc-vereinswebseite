9ba1b7d588e5a40cb2dadbb7ed3e25ee18af9197
Holger Ehrmann <holgerehrmann@gmail.com>
2025-08-03 10:45:49 +0200

    feat: display app version in footer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Process;
+use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,5 +27,9 @@ class AppServiceProvider extends ServiceProvider
             URL::forceScheme('https');
             $this->app['request']->server->set('HTTPS', true);
         }
+
+        $process = Process::run('git describe --tags --abbrev=0');
+        $version = $process->successful() ? trim($process->output()) : '0.0.0';
+        View::share('appVersion', $version);
     }
 }
