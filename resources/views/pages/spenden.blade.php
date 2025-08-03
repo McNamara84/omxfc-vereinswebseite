3f3831783a546d9171ec32594531b24683e703e9
McNamara <holgerehrmann@gmail.com>
2025-08-03 06:29:19 +0200

    Update spenden page text and form styling
    Clarified the description of the MADDRAX Fanclub to specify it as a postapocalyptic genre-mix series. Centered the PayPal donation form by adding 'text-center' to its class attribute.

--- resources/views/pages/spenden.blade.php
+++ resources/views/pages/spenden.blade.php
@@ -1,9 +1,9 @@
 <x-app-layout>
     <x-public-page>
         <h1 class="text-2xl sm:text-3xl font-bold text-[#8B0116] dark:text-[#ff4b63] mb-4 sm:mb-8">Spenden</h1>
-        <p class="mb-4">Der Offizielle MADDRAX Fanclub e. V. bietet Fans der Science-Fiction-Serie eine Plattform zum Austausch und zur gemeinsamen Organisation.</p>
+        <p class="mb-4">Der Offizielle MADDRAX Fanclub e. V. bietet Fans der postapokalyptischen Genre-Mix-Serie MADDRAX eine Plattform zum Austausch und zur gemeinsamen Organisation.</p>
         <p class="mb-6">Spenden helfen uns bei der Finanzierung der jährlichen Fantreffen sowie der Serverkosten dieser Webseite.</p>
-        <form action="https://www.paypal.com/donate" method="post" target="_top" class="mt-4">
+        <form action="https://www.paypal.com/donate" method="post" target="_top" class="mt-4 text-center">
             <input type="hidden" name="business" value="kassenwart@maddrax-fanclub.de" />
             <input type="hidden" name="no_recurring" value="0" />
             <input type="hidden" name="currency_code" value="EUR" />
