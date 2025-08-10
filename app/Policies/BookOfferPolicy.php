b1e1c6d74a4ccdefb9f7044f473163ce0488f00c
Holger Ehrmann <holgerehrmann@gmail.com>
2025-08-10 14:47:18 +0200

    refactor: drop deprecated HandlesAuthorization from book policies

--- app/Policies/BookOfferPolicy.php
+++ app/Policies/BookOfferPolicy.php
@@ -4,12 +4,9 @@ namespace App\Policies;
 
 use App\Models\BookOffer;
 use App\Models\User;
-use Illuminate\Auth\Access\HandlesAuthorization;
 
 class BookOfferPolicy
 {
-    use HandlesAuthorization;
-
     public function delete(User $user, BookOffer $offer): bool
     {
         return $user->id === $offer->user_id;
--- app/Policies/BookRequestPolicy.php
+++ app/Policies/BookRequestPolicy.php
@@ -4,12 +4,9 @@ namespace App\Policies;
 
 use App\Models\BookRequest;
 use App\Models\User;
-use Illuminate\Auth\Access\HandlesAuthorization;
 
 class BookRequestPolicy
 {
-    use HandlesAuthorization;
-
     public function delete(User $user, BookRequest $request): bool
     {
         return $user->id === $request->user_id;
