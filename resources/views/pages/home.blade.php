21c4861b0d9cd87f65ce0f8c918052487f1f439f
Holger Ehrmann <holgerehrmann@gmail.com>
2025-08-11 06:32:32 +0200

    Make chronik images clickable with modern formats

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -7,8 +7,12 @@
             <div class="md:col-span-2 bg-white dark:bg-gray-700 rounded-lg shadow-md overflow-hidden">
                 <div id="gallery" class="relative w-full h-48 sm:h-64 md:h-72">
                     @foreach($galleryImages as $image)
-                        <img loading="lazy" src="{{ asset($image) }}" alt="Foto von einem Treffen des Vereins mit einem Teil der Mitglieder"
-                            class="absolute inset-0 w-full h-full object-cover opacity-0 transition-opacity duration-1000">
+                        <picture>
+                            <source type="image/avif" srcset="{{ asset($image . '.avif') }}" />
+                            <source type="image/webp" srcset="{{ asset($image . '.webp') }}" />
+                            <img loading="lazy" src="{{ asset($image . '.webp') }}" alt="Foto von einem Treffen des Vereins mit einem Teil der Mitglieder"
+                                class="absolute inset-0 w-full h-full object-cover opacity-0 transition-opacity duration-1000">
+                        </picture>
                     @endforeach
                 </div>
             </div>
