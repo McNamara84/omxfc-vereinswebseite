77080748baefa5652039907094bb55b6c543eb81
Holger Ehrmann <holgerehrmann@gmail.com>
2025-08-16 17:05:42 +0200

    Fix colspan for empty audiobook episodes message

--- resources/views/hoerbuecher/index.blade.php
+++ resources/views/hoerbuecher/index.blade.php
@@ -56,7 +56,7 @@
                             </tr>
                         @empty
                             <tr>
-                                <td colspan="7" class="px-4 py-2 text-center text-gray-700 dark:text-gray-300">Keine Hörbuchfolgen vorhanden.</td>
+                                <td colspan="6" class="px-4 py-2 text-center text-gray-700 dark:text-gray-300">Keine Hörbuchfolgen vorhanden.</td>
                             </tr>
                         @endforelse
                     </tbody>
