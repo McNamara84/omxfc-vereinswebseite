d5f68311ab9fa9e7ab28710d9ad8ae7bac903c3c
Holger Ehrmann <holgerehrmann@gmail.com>
2025-09-09 04:51:02 +0200

    Mock Nominatim requests in tests (#298)
    * Mock Nominatim requests in tests
    
    * Clarify default Nominatim coordinates

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,25 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Http;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    /**
+     * Default coordinates used for stubbed Nominatim responses (Munich).
+     */
+    protected const DEFAULT_LAT = '48.0';
+    protected const DEFAULT_LON = '11.0';
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Http::fake([
+            'nominatim.openstreetmap.org/*' => Http::response([[
+                'lat' => self::DEFAULT_LAT,
+                'lon' => self::DEFAULT_LON,
+            ]], 200),
+        ]);
+    }
 }
