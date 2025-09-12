7f1c80f617363d5f2d282bbb329f2aba9c8b45cb
Holger Ehrmann <holgerehrmann@gmail.com>
2025-09-12 06:49:00 +0200

    fix: use built-in server in Playwright config

--- playwright.config.js
+++ playwright.config.js
@@ -3,9 +3,8 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   testDir: 'tests/e2e',
   webServer: {
-    command: 'php artisan serve --host=127.0.0.1 --port=8000',
+    command: 'php -S 127.0.0.1:8000 -t public public/index.php',
     url: 'http://127.0.0.1:8000',
-    port: 8000,
     reuseExistingServer: !process.env.CI,
     timeout: 180000,
   },
