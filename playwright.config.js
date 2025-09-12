7b6cdccb5f183f3e208515795c1bfd4011c64329
Holger Ehrmann <holgerehrmann@gmail.com>
2025-09-12 06:26:00 +0200

    Use artisan server in Playwright config

--- playwright.config.js
+++ playwright.config.js
@@ -3,10 +3,11 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   testDir: 'tests/e2e',
   webServer: {
-    command: 'php -S 127.0.0.1:8000 -t public',
+    command: 'php artisan serve --host=127.0.0.1 --port=8000',
     url: 'http://127.0.0.1:8000',
+    port: 8000,
     reuseExistingServer: !process.env.CI,
-    timeout: 120000,
+    timeout: 180000,
   },
   use: {
     baseURL: 'http://127.0.0.1:8000',
