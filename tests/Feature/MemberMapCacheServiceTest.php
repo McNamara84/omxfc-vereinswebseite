f932181efc495deccedd091da132a997edbdb732
Holger Ehrmann <holgerehrmann@gmail.com>
2025-09-12 05:05:17 +0200

    test: extend MemberMapCacheService scenarios

--- tests/Feature/MemberMapCacheServiceTest.php
+++ tests/Feature/MemberMapCacheServiceTest.php
@@ -80,5 +80,112 @@ class MemberMapCacheServiceTest extends TestCase
         $this->assertEquals((float) self::DEFAULT_LAT, $data['centerLat']);
         $this->assertEquals((float) self::DEFAULT_LON, $data['centerLon']);
     }
+
+    public function test_members_without_plz_or_with_anwaerter_role_are_excluded(): void
+    {
+        Cache::flush();
+
+        $team = Team::factory()->create();
+        $team->users()->detach();
+
+        $valid = User::factory()->create([
+            'plz' => '12345',
+            'stadt' => 'Valid City',
+            'lat' => 10.0,
+            'lon' => 20.0,
+            'current_team_id' => $team->id,
+        ]);
+        $anwaerter = User::factory()->create([
+            'plz' => '54321',
+            'stadt' => 'Skip City',
+            'lat' => 30.0,
+            'lon' => 40.0,
+            'current_team_id' => $team->id,
+        ]);
+        $noPlz = User::factory()->create([
+            'plz' => '',
+            'stadt' => 'NoPlz City',
+            'lat' => 50.0,
+            'lon' => 60.0,
+            'current_team_id' => $team->id,
+        ]);
+
+        $team->users()->attach($valid, ['role' => 'Mitglied']);
+        $team->users()->attach($anwaerter, ['role' => 'Anwärter']);
+        $team->users()->attach($noPlz, ['role' => 'Mitglied']);
+
+        $service = new MemberMapCacheService();
+
+        $data = $service->getMemberMapData($team);
+
+        $this->assertCount(1, $data['memberData']);
+        $member = $data['memberData'][0];
+        $this->assertEquals('Valid City', $member['city']);
+        $this->assertEquals(10.0, $data['centerLat']);
+        $this->assertEquals(20.0, $data['centerLon']);
+    }
+
+    public function test_jitter_is_applied_deterministically(): void
+    {
+        Cache::flush();
+
+        $team = Team::factory()->create();
+        $team->users()->detach();
+
+        $user = User::factory()->create([
+            'plz' => '11111',
+            'stadt' => 'Jitter City',
+            'lat' => 10.0,
+            'lon' => 20.0,
+            'current_team_id' => $team->id,
+        ]);
+
+        $team->users()->attach($user, ['role' => 'Mitglied']);
+
+        $service = new MemberMapCacheService();
+
+        mt_srand(1234);
+        $data = $service->getMemberMapData($team);
+
+        mt_srand(1234);
+        $expectedLat = 10.0 + (mt_rand(-50, 50) / 10000);
+        $expectedLon = 20.0 + (mt_rand(-50, 50) / 10000);
+
+        $this->assertEqualsWithDelta($expectedLat, $data['memberData'][0]['lat'], 0.000001);
+        $this->assertEqualsWithDelta($expectedLon, $data['memberData'][0]['lon'], 0.000001);
+    }
+
+    public function test_refresh_generates_new_jitter(): void
+    {
+        Cache::flush();
+
+        $team = Team::factory()->create();
+        $team->users()->detach();
+
+        $user = User::factory()->create([
+            'plz' => '22222',
+            'stadt' => 'Refresh City',
+            'lat' => 10.0,
+            'lon' => 20.0,
+            'current_team_id' => $team->id,
+        ]);
+
+        $team->users()->attach($user, ['role' => 'Mitglied']);
+
+        $service = new MemberMapCacheService();
+
+        mt_srand(1);
+        $first = $service->getMemberMapData($team);
+        $firstLat = $first['memberData'][0]['lat'];
+        $firstLon = $first['memberData'][0]['lon'];
+
+        mt_srand(2);
+        $second = $service->refresh($team);
+        $secondLat = $second['memberData'][0]['lat'];
+        $secondLon = $second['memberData'][0]['lon'];
+
+        $this->assertNotEquals($firstLat, $secondLat);
+        $this->assertNotEquals($firstLon, $secondLon);
+    }
 }
 
