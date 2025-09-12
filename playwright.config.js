17d026d7d10dfc473d9ffcd745559a4e007a2f65
Holger Ehrmann <holgerehrmann@gmail.com>
2025-09-12 05:52:51 +0200

    Fix Playwright server setup for CI

--- playwright.config.js
+++ playwright.config.js
@@ -3,9 +3,10 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   testDir: 'tests/e2e',
   webServer: {
-    command: 'php artisan serve --port=8000',
+    command: 'php artisan serve --host=127.0.0.1 --port=8000',
     url: 'http://127.0.0.1:8000',
     reuseExistingServer: !process.env.CI,
+    timeout: 120000,
   },
   use: {
     baseURL: 'http://127.0.0.1:8000',
