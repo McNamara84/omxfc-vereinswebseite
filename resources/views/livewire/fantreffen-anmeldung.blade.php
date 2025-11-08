6565a4ecbe98aa4f820d0692b4142954064c5bb2
McNamara <holgerehrmann@gmail.com>
2025-11-08 17:36:04 +0100

    FIX: Remove @change event and required attr to allow Livewire form submission

--- resources/views/livewire/fantreffen-anmeldung.blade.php
+++ resources/views/livewire/fantreffen-anmeldung.blade.php
@@ -119,8 +119,7 @@
                                             <input type="checkbox" 
                                                    x-model="tshirtChecked"
                                                    wire:key="tshirt-checkbox" 
-                                                   class="w-5 h-5 mt-0.5"
-                                                   @change="$wire.set('tshirt_bestellt', tshirtChecked)">
+                                                   class="w-5 h-5 mt-0.5">
                                             <div>
                                                 <span class="font-medium">Event-T-Shirt bestellen</span>
                                                 <p class="text-xs text-gray-500 mt-1">25,00 € Spende{{ !$isLoggedIn ? ' (zusammen mit Teilnahme: 30,00 €)' : '' }}</p>
@@ -129,7 +128,7 @@
                                         
                                         <div x-show="tshirtChecked" class="mt-3" wire:key="tshirt-size-dropdown" x-transition>
                                             <label class="block text-sm font-medium mb-2">T-Shirt-Größe *</label>
-                                            <select wire:model.live="tshirt_groesse" wire:key="tshirt-groesse-select" class="w-full px-3 py-2 border rounded dark:bg-gray-700" required>
+                                            <select wire:model="tshirt_groesse" wire:key="tshirt-groesse-select" class="w-full px-3 py-2 border rounded dark:bg-gray-700">
                                                 <option value="">Bitte wählen...</option>
                                                 <option value="XS">XS</option>
                                                 <option value="S">S</option>
@@ -139,6 +138,7 @@
                                                 <option value="XXL">XXL</option>
                                                 <option value="XXXL">XXXL</option>
                                             </select>
+                                            @error('tshirt_groesse') <span class="text-red-600 text-sm mt-1">{{ $message }}</span> @enderror
                                         </div>
                                     </div>
                                 </div>
