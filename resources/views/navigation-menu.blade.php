9afe9c250d6e7d859cf0fc45d285c9173f3ddd18
McNamara <holgerehrmann@gmail.com>
2026-02-21 16:45:15 +0100

    Adjust desktop navigation alignment
    Update desktop menu container classes to use justify-between instead of justify-center and remove the xl:gap-1 spacing. This changes the layout so items are distributed across the available space on xl screens (resources/views/navigation-menu.blade.php).

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -5,7 +5,7 @@
         </a>
 
         {{-- Desktop-Menü --}}
-        <div class="hidden flex-1 justify-center xl:flex xl:items-center xl:gap-1">
+        <div class="hidden flex-1 justify-between xl:flex xl:items-center">
             @auth
                 <x-button label="Dashboard" link="{{ route('dashboard') }}" class="btn-ghost btn-sm" />
                 <x-button label="Fantreffen 2026" link="{{ route('fantreffen.2026') }}" class="btn-ghost btn-sm" />
