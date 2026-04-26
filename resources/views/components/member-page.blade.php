1c3cc1c694d8f97a876309f5090554507cbc7d40
McNamara <holgerehrmann@gmail.com>
2026-04-26 15:54:38 +0200

    Use Tailwind max-w-7xl in member page
    Update resources/views/components/member-page.blade.php to replace the custom max-w-[88rem] class with Tailwind's max-w-7xl utility. This standardizes the component's container width to the project's design tokens and simplifies the utility classes.

--- resources/views/components/member-page.blade.php
+++ resources/views/components/member-page.blade.php
@@ -1,3 +1,3 @@
-<div {{ $attributes->merge(['class' => 'mx-auto w-full max-w-[88rem] px-4 pb-14 pt-4 sm:px-6 sm:pb-16 lg:px-8 lg:pt-6']) }}>
+<div {{ $attributes->merge(['class' => 'mx-auto w-full max-w-7xl px-4 pb-14 pt-4 sm:px-6 sm:pb-16 lg:px-8 lg:pt-6']) }}>
     {{ $slot }}
 </div>
